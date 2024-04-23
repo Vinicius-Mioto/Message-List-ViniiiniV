f5fa5a3f2cf33d9d4e865bdf9f37fb32c21e6b4b
ViniiiniV <viniciusbmioto@gmail.com>
2024-04-23 14:46:44 -0300

    small changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     return(
         <header className="container-header">
             <div className={styles.headerBox}>
-                <h1>Message List DOM</h1>
+                <h1>Message List</h1>
                 <nav>
                     <a href="https://github.com/Vinicius-Mioto/Message-List-ViniiiniV" target="_blank"><FaGithub size={35} /></a>
                 </nav>
